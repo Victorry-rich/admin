4f985910642ca0052658c626a902708c8a0497bd
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-11-03 16:24:41 +0100

    fix the dependencies

--- src/admin/component-loader.ts
+++ src/admin/component-loader.ts
@@ -4,10 +4,10 @@ const componentLoader = new ComponentLoader();
 
 // Register your custom components with camelCase naming (no hyphens!)
 const Components = {
-  Dashboard: componentLoader.add('Dashboard', './components/dashboard'),
   CloudinaryUpload: componentLoader.add('CloudinaryUpload', './components/cloudinary-upload'),
   CloudinaryImage: componentLoader.add('CloudinaryImage', './components/cloudinary-image'),
   JsonEditor: componentLoader.add('JsonEditor', './components/json-editor'),
+  Dashboard: componentLoader.add('Dashboard', './components/dashboard'),
 };
 
 export { componentLoader, Components };
